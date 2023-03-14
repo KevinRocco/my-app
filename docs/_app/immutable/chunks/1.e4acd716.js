c77f0735726231e83f43988efa3933cb6df5c74e
kntryer <1162676656@qq.com>
2023-03-15 00:43:21 +0800

    1

---
+++ docs/_app/immutable/chunks/1.e4acd716.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ca58ebb7.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,7 @@ const config = {
         }),
         paths: {
             base: dev ? "" : "/my-app",
+			relative: false
         },
     },
 };
